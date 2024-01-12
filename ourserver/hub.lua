838697f855918945cea04083af22d7defafb35c6
kfred93 <kfred93@hotmail.com>
2024-01-12 01:25:24 -0800

    Update hub.lua

--- ourserver/hub.lua
+++ ourserver/hub.lua
@@ -60,7 +60,7 @@ local function run()
 	if message.compLabel ~= nil then
 		if message.action == "settings" then 
 			print("Transmitting settings to " .. message.compLabel)
-			modem.transmit(senderChannel, replyChannel, getSettings(message.compLabel)
+			modem.transmit(senderChannel, replyChannel, getSettings(message.compLabel))
 		end
 	end
 end
